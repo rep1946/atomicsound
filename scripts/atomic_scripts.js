50a398a2d7db400cec316f416a5ee24195bdf9f7
rep1946 <rep1946@me.com>
2018-03-22 08:29:26 -0400

    Committ 3
    Added mouse events to hamburger, icons

--- scripts/atomic_scripts.js
+++ scripts/atomic_scripts.js
@@ -1,1 +1,50 @@
 console.log('Im connected');
+
+
+$(document).ready(() => {
+  
+  // $('.more-details-button').on('click', event => {
+  //   $(event.currentTarget).closest('.product-details').next().toggle();
+
+
+	//  $('.login-button').on('click', () => {
+	//     $('.login-form').toggle();
+	//     $('.login-button').toggleClass('button-active');
+	// });
+
+	// $( "#other" ).click(function() {
+	// 	$( "#target" ).click();
+	// });
+
+
+
+	$('#hamburgerMenu').on('click', () => {
+		$('.mobile_dropdown').show();
+	});
+
+
+	$('#hamburgerMenu').on('click', () => {
+
+		$('.mobile_dropdown').hide();
+
+	});
+
+
+
+
+});
+
+
+
+
+
+ // $('.login-button').on('click', () => {
+ //    $('.login-form').show();
+ //  })
+
+ // $('.nav-menu').on('mouseleave', () => {
+ //    $('.nav-menu').hide();
+ //  })
+
+
+
